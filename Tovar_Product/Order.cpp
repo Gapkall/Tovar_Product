dc939a114d3589933917f6b1b74c848f7ddce696
RohachOlesia <olesunia04032007@gmail.com>
2025-03-22 14:14:50 +0200

    add implementation to header files

--- Tovar_Product/Order.cpp
+++ Tovar_Product/Order.cpp
@@ -1,22 +1,29 @@
+
 #include "Order.h"
+#include <iostream>
 
-Order::Order() : order_amount(0)
-{
+using namespace std;
+
+Order::Order() : Product(), order_amount(0) {}
+
+Order::Order(string name, double price, string currency, int amount)
+    : Product(name, price, currency), order_amount(amount) {
 }
 
-Order::Order(double price) : order_amount(order)
-{
+Order::Order(const Order& O) : Product(O), order_amount(O.order_amount) {}
+
+Order::~Order() {}
+
+void Order::print() const {
+    cout << "Order: " << product_name
+        << ", Price: " << product_price << " " << product_currency
+        << ", Amount: " << order_amount << endl;
 }
 
-Order::Order(Order& O) : order_amount(O.order_amount)
-{
+double Order::calculateOrder() const {
+    return product_price * order_amount;
 }
 
-Order::~Order()
-{
-}
-
-bool Order::operator<(const Order& O)
-{
-	return calculateOrder() < O.calculateOrder();
+bool Order::operator<(const Order& O) const {
+    return order_amount < O.order_amount;
 }
--- Tovar_Product/Product.cpp
+++ Tovar_Product/Product.cpp
@@ -2,43 +2,62 @@
 #include <string>
 #include "Product.h"
 
+using namespace std;
+
 Product::Product() : product_name(""), product_price(0.0), product_currency("") {}
 
-Product::Product(string name, double price, string currency) : product_name(name), product_price(price), product_currency(currency) {}
+Product::Product(string name, double price, string currency)
+    : product_name(name), product_price(price), product_currency(currency) {
+}
 
-Product::Product(const Product& P) : product_name(P.product_name), product_price(P.product_price), product_currency(P.product_currency) {}
+Product::Product(const Product& P)
+    : product_name(P.product_name), product_price(P.product_price), product_currency(P.product_currency) {
+}
 
 Product::~Product() {}
 
-void Product::convertCurrency(double exchangeRate, string newCurrency)
-{
-	product_price *= exchangeRate;
-	product_currency = newCurrency;
+Product& Product::operator=(const Product& P) {
+    if (this != &P) {
+        product_name = P.product_name;
+        product_price = P.product_price;
+        product_currency = P.product_currency;
+    }
+    return *this;
 }
 
-void Product::readFrom(istream& in)
-{
-	in >> product_price;
+void Product::convertCurrency(double exchangeRate, string newCurrency) {
+    if (exchangeRate > 0) {
+        product_price *= exchangeRate;
+        product_currency = newCurrency;
+    }
+    else {
+        cerr << "Error: Invalid exchange rate!" << endl;
+    }
 }
 
-void Product::writeTo(ostream& out) const
-{
-	out << product_price;
+void Product::readFrom(istream& in) {
+    in >> product_name >> product_price >> product_currency;
 }
 
-Product Product::operator+(double amount) const
-{
-	return Product(product_name, product_price + amount, product_currency);
+void Product::writeTo(ostream& out) const {
+    out << "Product: " << product_name
+        << ", Price: " << product_price << " " << product_currency;
 }
 
-istream& operator>>(istream& in, Product& P)
-{
-	P.readFrom(in);
-	return in;
+Product Product::operator+(double amount) const {
+    return Product(product_name, product_price + amount, product_currency);
 }
 
-ostream& operator<<(ostream& out, const Product& P)
-{
-	P.writeTo(out);
-	return out;
+Product Product::operator-(double amount) const {
+    return Product(product_name, product_price - amount, product_currency);
+}
+
+istream& operator>>(istream& in, Product& P) {
+    P.readFrom(in);
+    return in;
+}
+
+ostream& operator<<(ostream& out, const Product& P) {
+    P.writeTo(out);
+    return out;
 }
